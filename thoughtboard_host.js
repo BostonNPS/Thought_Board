656665bc4457b19a4f1b257e274c3cf74e1288d4
Eric Hanson Plass <emh5004@gmail.com>
2017-01-13 17:43:50 +0000

    static file hosting added

--- thoughtboard_host.js
+++ thoughtboard_host.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -41,9 +42,7 @@ app.get('/app', function(req, res){
   res.sendFile(__dirname + '/thoughtboard_client.html');
 });
 
-app.get('/manifest.json', function(req, res){
-  res.sendfile(__dirname + 'manifest.json');
-});
+app.use(express.static(__dirname));
 
 io.on('connection', function(socket){
 	io.emit('data',JSON.stringify(query))
